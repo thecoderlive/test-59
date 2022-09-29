6397970e97f868d9045b879fddea165f524f454b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 15:50:13 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "4ff650d5-f8a5-4e3d-b09d-dffeac191514",
+    "home_feed": [
+        {
+            "id": "4c9c0e39-d674-4a78-850f-ca64c01801b2",
+            "artist_name": "action",
+            "home_carousel": [
+                {
+                    "id": "12e7387b-3dea-4589-a445-6de16f0e5a28",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "f7164450-8326-48c9-8d30-e6eb0b48604b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "14feede6-a9a1-4523-a82e-21648f609085",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "61252987-e735-4ec6-ba4b-837db1df34d7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "4ba52fe3-721c-4d90-a1d2-31f8f56dbb69",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                }
+            ],
+            "art_caption": "figure",
+            "art_description": "Compare star nice left. Hit me design effort."
+        },
+        {
+            "id": "a8f0ac95-25cb-4bf4-ba1d-b4199246a52c",
+            "artist_name": "walk",
+            "home_carousel": [
+                {
+                    "id": "ba5ad944-0540-41d9-ac42-604ee0b62251",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "697192f2-af8b-4a39-bbc7-44f3b232a214",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "8a6fb385-4ef0-46c8-9404-5ce2bd3fbb34",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                },
+                {
+                    "id": "ba789932-4802-48f9-b223-d4c4acf0c396",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "07a426bc-617a-4aca-8e08-aa7d49453a73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                }
+            ],
+            "art_caption": "pull",
+            "art_description": "Three establish shake add than difficult. Long increase chance usually series stage. Total loss store stand pattern show."
+        },
+        {
+            "id": "e67e0884-428c-40db-b3aa-a2b71eaf5be6",
+            "artist_name": "space",
+            "home_carousel": [
+                {
+                    "id": "56547029-2625-43b1-ad12-af2ab95e0143",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                },
+                {
+                    "id": "267e1238-45c5-4efb-a378-a0a1380d430e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                },
+                {
+                    "id": "ff06f9df-333a-4a65-b7a1-23bb29522ee4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "e4264ed5-1f95-40f3-890d-bc130552ced3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                },
+                {
+                    "id": "246cdc95-5d45-44a6-bbd6-8b358de378b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                }
+            ],
+            "art_caption": "design",
+            "art_description": "Author throughout while off minute house walk."
+        },
+        {
+            "id": "4427de44-e688-4f0e-9d85-3687594b4787",
+            "artist_name": "onto",
+            "home_carousel": [
+                {
+                    "id": "e9c836f5-268c-4320-8ab8-0326f04159cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                },
+                {
+                    "id": "cf9f38ec-5bc3-405c-86fe-3bf500c2ebac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                },
+                {
+                    "id": "937010d4-2036-480a-a3c1-73470b7c1830",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                },
+                {
+                    "id": "106715b5-f3ce-4cf4-840f-021aedf351ea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "2438cb9b-6d21-452b-8d64-3cb073f1859f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                }
+            ],
+            "art_caption": "be",
+            "art_description": "Red drop in avoid. Real fact war network look. Year run industry people among bank."
+        },
+        {
+            "id": "540a5f9b-0812-406e-a0b8-a6d99b5f871d",
+            "artist_name": "music",
+            "home_carousel": [
+                {
+                    "id": "945661a5-9300-45ac-aee5-0855e4c4b32e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "cc1d8252-389e-45c1-be6b-12a50a19d472",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "9e28d7aa-8303-4563-94b0-a76d8b0e3644",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/67.png"
+                },
+                {
+                    "id": "39bf9992-d3df-4e93-8e3b-10a43a104f93",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "a07346e1-b793-4e99-ae27-c2c319dfeb4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+                }
+            ],
+            "art_caption": "off",
+            "art_description": "Blue wind soon last so customer. Section thought spend different."
+        }
+    ]
+}
